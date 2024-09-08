7af7750986c71f3e72b8ebddfbfa4111c6d47eff
acer <leonv.allotey@gmail.com>
2024-09-07 19:45:57 -0500

    updated main.cs and README.md

--- FileConsolidator/README.md
+++ FileConsolidator/README.md
@@ -1,5 +1,9 @@
-A command-line program that takes an output path and list of .cs files, and combines the .cs files into a single, output .cs file. 
+A command-line program that takes an output path and list of text-based files, and combines the files into a single, output file of a specified file extension. 
 
-The 0th argument is the output file path. It can be a path to an existing directory (as Program.cs), an existing file, a new directory (as Program.cs), a new file in a new directory, or a file name (automatically placed in current working directory). The following arguments (1, 2..) must be paths to the files that are to be merged/consolidated into the output file. An exception is thrown if any of the files/directories are invalid, inaccessible, etc.
+All of the arguments must be paths to the files that are to be merged/consolidated into the output file. An exception is thrown if any of the files/directories are invalid, inaccessible, etc.
 
-Designed for Windows. Untesed on Mac and linux.
+The *".o="* modifer must be a string to the output file path. It can be a path to an existing/real directory (as *MergedFile.extension*), an existing file, a new directory (as *MergedFile.extension*), a new file in a new directory, or a file name (automatically placed in current working directory). The default value is the path to the current working directory. 
+
+The *".extension="*, *".ext="* modifer must be a string that represents a valid file extension. It can be prepended with or without a period (*".txt"*, *"txt"*). The default value is *".txt"*.
+
+Designed for Windows with .NET 8.0.401. Untested on macOS and Linux.
--- FileConsolidator/main.cs
+++ FileConsolidator/main.cs
@@ -5,44 +5,118 @@ using System.Linq;
 
 namespace FileConsolidator
 {
-	class Program
+	
+class Program
+{
+	static string? ParseMod(string[] argumentList, string mod)
 	{
-		static void Main(string[] args)
+		string? res = null;
+		foreach (string arg in argumentList)
 		{
-			string outputFileName = "Program";
-			string outputFilePath;
-			if (Directory.Exists(args[0])) {
-				outputFilePath = args[0] + '\\' + outputFileName;
-			} else if (File.Exists(args[0])) {
-				outputFilePath = args[0];
-			} else if (args[0].EndsWith('\\') || args[0].EndsWith('/') ) {	
-				Directory.CreateDirectory(args[0]);
-				outputFilePath = args[0] + outputFileName;
-			} else if (!args[0][1..].Contains('\\') && !args[0][1..].Contains('/')) {	
-				if ( args[0].StartsWith('\\') || args[0].StartsWith('/') ) {
-					outputFilePath = Directory.GetCurrentDirectory() + @"\" + args[0];
-				} else {
-					outputFilePath = Directory.GetCurrentDirectory() + @"" + args[0];
-				}
-			} else if (true) {
-				Directory.CreateDirectory(Path.GetDirectoryName(args[0]));
-				outputFilePath = args[0];
-			} else {
-				throw new Exception("Invalid Path Exception");
+			if (arg.StartsWith(mod)){
+				res = arg.Split('=')[1];
+				break;
 			}
-			outputFilePath = Path.ChangeExtension(outputFilePath, ".cs");
-			Console.WriteLine($"Output path: {outputFilePath}");
+		}
 
-			// Parse input files
-			Console.WriteLine($"Parsing files...");
-			List<string> inputFilePaths = new(args[1..]);
-			List<string> inputFileLines = [];
-			List<string> usingStatements = [];
-			
-			foreach (string path in inputFilePaths)
+		if (res == "")
+			throw new Exception($"Exception: Modifer \"{mod[..mod.IndexOf('=')]}\" cannot be empty.");
+
+		return res;
+	}
+	static string PathConditionInternal(string path)
+	{
+		char dirSep = Path.DirectorySeparatorChar;
+		if (Directory.Exists(path))
+			return "IsRealDirectory";
+		else if (File.Exists(path))
+			return "IsRealFile";
+		else if (path.EndsWith(dirSep))
+			return "IsFakeDirectory";
+		else if ((!path.Contains(dirSep)) || (path.StartsWith(dirSep) && !path[1..].Contains(dirSep)))
+			return "IsFileName";
+		else 
+			return "IsFakeFilePath";
+	}
+	
+	static bool IsRealDirectory(string path) => nameof(IsRealDirectory).Equals(PathConditionInternal(path));
+	static bool IsRealFile(string path) => nameof(IsRealFile).Equals(PathConditionInternal(path));
+	static bool IsFakeDirectory(string path) => nameof(IsFakeDirectory).Equals(PathConditionInternal(path));
+	static bool IsFileName(string path) => nameof(IsFileName).Equals(PathConditionInternal(path));
+	static bool IsFakeFilePath(string path) => nameof(IsFakeFilePath).Equals(PathConditionInternal(path));
+	
+	static void Main(string[] args)
+	{
+		// Get input files
+		string[] mods = [".ext=", ".extension=", ".o="];
+		string[] filesToCombine = (from arg in args where !mods.Contains(arg[..(arg.IndexOf('=')+1)]) select arg).ToArray();
+		
+		// Set extension to out file
+		string defaultFileExtension = ".txt";
+		string fileExt = defaultFileExtension;
+		foreach (string mod in new string[2] {".ext=", ".extension="})
+		{
+			fileExt = ParseMod(args, mod) ?? fileExt;
+		}
+
+		// Set path to output file
+		string defaultFilePath = Directory.GetCurrentDirectory();
+		string filePath = defaultFilePath;
+		filePath = ParseMod(args, ".o=") ?? filePath;
+		
+		// Parse the output file path
+		string defaultOutputFileName = "MergedFile";
+		string outputFilePath;
+		if (IsRealDirectory(filePath)) 
+		{
+			outputFilePath = Path.Join(filePath, defaultOutputFileName);
+		} else if (IsRealFile(filePath)) 
+		{
+			outputFilePath = filePath;
+		} else if (IsFakeDirectory(filePath)) 
+		{	
+			Console.WriteLine($"Creating Directory \"{filePath}\"...");
+			Directory.CreateDirectory(filePath);
+			Console.WriteLine($"Created Directory \"{filePath}\".");
+			outputFilePath = Path.Join(filePath, defaultOutputFileName);
+		} else if (IsFileName(filePath)) 
+		{	
+			outputFilePath = Path.Join(Directory.GetCurrentDirectory(), filePath);
+		} else if (IsFakeFilePath(filePath)) {
+			Console.WriteLine($"Creating Directory \"{Path.GetDirectoryName(filePath)}\"...");
+			Directory.CreateDirectory(Path.GetDirectoryName(filePath) ?? defaultFilePath);
+			Console.WriteLine($"Created Directory \"{Path.GetDirectoryName(filePath)}\".");
+			outputFilePath = filePath;
+		} else {
+			throw new Exception($"Exception: Invalid path.");
+		}
+		
+		try {
+			outputFilePath = Path.ChangeExtension(outputFilePath, fileExt);
+		} catch (Exception)
+		{
+			throw new Exception($"Exception: Invalid file extension.");
+		}
+		Console.WriteLine($"Output path: {outputFilePath}");
+
+		// Combine input files
+		Console.WriteLine($"Parsing files...");
+		List<string> inputFilePaths = new(filesToCombine);
+		List<string> inputFileLines = [];
+		List<string> usingStatements = [];
+		
+		foreach (string path in inputFilePaths)
+		{
+			string[] fileContent;
+			try 
 			{
-				string[] fileContent = File.ReadAllLines(path);
-				inputFileLines.AddRange(fileContent);
+				fileContent = File.ReadAllLines(path);
+			} catch (Exception) 
+			{ 
+				throw new Exception($"Exception: \"{path}\" is not a valid path."); 
+			}
+			inputFileLines.AddRange(fileContent);
+			if (fileExt == ".cs" || fileExt == "cs"){
 				foreach (string inputFileLine in fileContent)
 				{
 					if (inputFileLine.StartsWith("using")) 
@@ -51,9 +125,11 @@ namespace FileConsolidator
 					}
 				}
 			}
-
-			// Parse using statements
-			List<string> parsedUsingStatements = [];
+		}
+	
+		// Parse using statements (if extension is ".cs")
+		List<string> parsedUsingStatements = [];
+		if (fileExt == ".cs" || fileExt == "cs"){
 			string[] inputLinesCopy = [.. inputFileLines];
 			foreach (string line in inputLinesCopy)
 			{
@@ -77,16 +153,17 @@ namespace FileConsolidator
 					inputFileLines.Remove(line);
 				}
 			}
-			Console.WriteLine($"Files parsed.");
-
-            // Concatenate the using statements and other code
-			Console.WriteLine($"Concatenating text...");
-			List<string> outputFileContent = new List<string>(parsedUsingStatements.Concat(inputFileLines));
-			Console.WriteLine($"Concatenated text.");
-            // Create File and Write the content
-			Console.WriteLine($"Creating and writing to file \"{outputFilePath}\"...");
-			File.WriteAllLines(outputFilePath, outputFileContent);
-			Console.WriteLine($"Created and wrote to file \"{outputFilePath}\".");
 		}
+		Console.WriteLine($"Files parsed.");
+
+		// Concatenate the using statements and other code
+		Console.WriteLine($"Concatenating text...");
+		List<string> outputFileContent = new(parsedUsingStatements.Concat(inputFileLines));
+		Console.WriteLine($"Concatenated text.");
+		// Create File and Write the content
+		Console.WriteLine($"Creating and writing to file \"{outputFilePath}\"...");
+		File.WriteAllLines(outputFilePath, outputFileContent);
+		Console.WriteLine($"Created and wrote to file \"{outputFilePath}\".");
 	}
+}
 }
